44d6caffc321f3c987642189baaba374060793e2
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-08-11 16:16:14 +0100

    Update weather.sh

--- polybar/scripts/weather.sh
+++ polybar/scripts/weather.sh
@@ -2,10 +2,16 @@
 
 #curl wttr.in/Caerphilly?format="%l+%C+%f" > /home/phil/.config/polybar/scripts/weatherinfo
 
-echo $(curl wttr.in/Caerphilly?format="%l+%C+%f" 2>/dev/null | cut -d' ' -f2- )
-#sleep 0.25
-
-#mystring="$(cat /home/phil/.config/polybar/scripts/weatherinfo)"
+##echo $(curl wttr.in/Caerphilly?format="%l+%C+%f" 2>/dev/null | cut -d' ' -f2- )
 
 
-#echo $(cat /home/phil/.config/polybar/scripts/weatherinfo)
+weath=$(curl wttr.in/Caerphilly?format="%l+%C+%f" 2>/dev/null | cut -d' ' -f2-)
+echo $weath
+
+if [[ $weath == *location* || $weath == *running* ]]; then
+  echo "<Unavailable>"
+  
+else
+  echo $weath
+fi
+
