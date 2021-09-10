8b08daee8b2c33918cea93e42c9129443e1dd8ef
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-09-10 09:47:56 +0100

    Update 130_create_directories.sh

--- 130_create_directories.sh
+++ 130_create_directories.sh
@@ -180,6 +180,30 @@ cd i3blocks
 cp -r * ~/.config/i3blocks
 cd ..
 
+######## Install xmonad ##########################
+mkdir -p ~/.xmonad
+mkdir -p ~/.config/xmobar
+
+cd xmonad
+chmod +x autostart.sh
+#chmod +x testwallpaper
+chmod +x xmonad.hs
+cd ..
+
+cd xmobar
+chmod +x xmobarrc
+chmod +x *.sh
+
+cd ..
+
+cd xmonad
+cp -r * ~/.xmonad
+cd ..
+
+cd xmobar
+cp -r * ~/.config/xmobar
+cd ..
+
 ######## Install DWM ##############################
 
 mkdir -p ~/.local/share/dwm
