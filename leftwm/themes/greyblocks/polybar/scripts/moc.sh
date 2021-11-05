fd69f6a933e33bbee92226b024f8d02942ae7b95
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-11-05 08:55:20 +0000

    Add files via upload

--- leftwm/themes/greyblocks/polybar/scripts/moc.sh
+++ leftwm/themes/greyblocks/polybar/scripts/moc.sh
@@ -11,11 +11,11 @@ state=$state"f"
 #echo $state
 if [ $state == "PLAYf" ]; then
 	icon=" "
-	this=$(mocp -Q %file 2>/dev/null | sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,20)}')
+	this=$(mocp -Q %file 2>/dev/null | sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g'| awk '$1>0 {print substr($1,1,22)}')
 	this=$this"..."
 elif [ $state == "PAUSEf" ]; then
 	icon=" "
-	this=$(mocp -Q %file 2>/dev/null| sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,20)}')
+	this=$(mocp -Q %file 2>/dev/null| sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g'| awk '$1>0 {print substr($1,1,22)}')
 	this=$this"..."
 else
 	icon=" "
