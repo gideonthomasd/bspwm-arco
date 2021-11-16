654b07d610068d1c74fc9e4f01652f96ecb2ef5b
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-11-16 20:22:55 +0000

    Add files via upload

--- polybar-bspwm/scripts/moc.sh
+++ polybar-bspwm/scripts/moc.sh
@@ -11,11 +11,11 @@ state=$state"f"
 #echo $state
 if [ $state == "PLAYf" ]; then
 	icon=" "
-	this=$(mocp -Q %file 2>/dev/null | sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,8)}')
+	this=$(mocp -Q %file 2>/dev/null | sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g'| awk '$1>0 {print substr($1,1,15)}')
 	this=$this"..."
 elif [ $state == "PAUSEf" ]; then
 	icon=" "
-	this=$(mocp -Q %file 2>/dev/null| sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,8)}')
+	this=$(mocp -Q %file 2>/dev/null| sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g'| awk '$1>0 {print substr($1,1,15)}')
 	this=$this"..."
 else
 	icon=" "
